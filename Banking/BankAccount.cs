203f8dc093114c1f230c313c942e82f98917ba6a
Shane Wilson <new@mail.com>
2021-10-13 17:09:03 +0100

    Initial demo banking code for exception handling

---
+++ Banking/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class BankAccount
+    {
+        // Properties
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+
+        }
+        private static int accountNumberSeed = 1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        // Constructor
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+        }
+
+        // Functions
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Testing Code
+
+            var account = new BankAccount("Milo", 1000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} dollars");
+
+            decimal depositAmount = 200.0m;
+            account.MakeDeposit(200, DateTime.Now, "Cash deposit made in person");
+
+            Console.WriteLine($"Deposit of {depositAmount } euros made to Account {account.Number}. New balance of {account.Balance} euros");
+
+
+        }
+    }
+}
